a5358a0a6ae0a03f4be4dfb91ac51ab35619dc63
rokan <rokansubhi.f@gmail.com>
2023-02-08 08:39:24 +0700

    PPH21

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => 'pph21/calculator',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'name' => 'perhitungan-pajak21/calculator',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '09d6af3ab78ad7454cdc1e2917c0a8ee044c539e',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        'pph21/calculator' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+        'perhitungan-pajak21/calculator' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '09d6af3ab78ad7454cdc1e2917c0a8ee044c539e',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
